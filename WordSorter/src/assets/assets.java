5c5982c116b7b94b3c6f91b29a0b85eb8cb00100
paulm <paulm@DESKTOP-83U6OL9.fritz.box>
2020-06-19 23:30:31 +0200

    Update

---
+++ WordSorter/src/assets/assets.java
@@ -0,0 +1,14 @@
+package assets;
+
+import java.io.File;
+
+public class assets {
+
+	public static File getFile(String string) {
+		System.err.println("Methode nich da getFile in assets");
+		return null;
+	}
+	
+	
+	
+}
---
+++ WordSorter/src/me/paul/WordSorter/listener/JTextInput.java
@@ -0,0 +1,48 @@
+package me.paul.WordSorter.listener;
+
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.Element;
+import javax.swing.text.Position;
+import javax.swing.text.Segment;
+
+import me.paul.WordSorter.gui.Gui;
+import me.paul.api.DragDrop.JElements.JMoveComponent;
+
+public class JTextInput extends JTextField {
+	
+	JTextInput instance = this;
+	
+	public JTextInput(Color linec, String st) {
+		super(st.length());
+		setDocument(new JLimitDocument(st.length()));
+		setBackground(linec);
+		addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (st.equalsIgnoreCase(getText())) {
+					disable();
+					setVisible(false);
+					
+					JLabel l = new JLabel(st);
+					Rectangle rec = JMoveComponent.getSize(st, Gui.f);
+					Gui.jf.add(l);
+				} else {
+					setForeground(Color.RED);
+				}
+			}
+		});
+	}
+}
